9732e9928535f5a30efb5ab581378d2143b9d799
Duygu <www.duyguyildirim@gmail.com>
2023-02-03 20:43:46 +0300

    add: text adventure state

---
+++ Assets/_GAME/Scripts/States/TextAdventureState.cs
@@ -0,0 +1,17 @@
+using Ambrosia.StateMachine;
+
+namespace _GAME.Scripts.States
+{
+    public class TextAdventureState : State
+    {
+        protected override void OnEnter()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnExit()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
