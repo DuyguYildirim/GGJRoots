80fe3b13fd6ee12677d56915442e06c257546efd
Duygu <www.duyguyildirim@gmail.com>
2023-02-05 16:40:27 +0300

    wip

---
+++ Assets/_GAME/Scripts/Events/FpsGameWinEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace _GAME.Scripts.Events
+{
+    public struct FpsGameWinEvent
+    {
+        
+    }
+}
--- Assets/_GAME/Scripts/States/FpsGameState.cs
+++ Assets/_GAME/Scripts/States/FpsGameState.cs
@@ -1,20 +1,48 @@
-﻿using Ambrosia.StateMachine;
+﻿using System.Collections;
+using _GAME.Scripts.Events;
+using Ambrosia.EventBus;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using State = Ambrosia.StateMachine.State;
 
 namespace _GAME.Scripts.States
 {
     public class FpsGameState : State
     {
         [SerializeField] private GameObject fpsGame;
+        [SerializeField] private SkyrimAdventureState _skyrimAdventureState;
+        [SerializeField] private TextAdventureState _textAdventureState;
 
         protected override void OnEnter()
         {
             fpsGame.SetActive(true);
+            StartCoroutine(WaitAndPrint(600));
+            EventBus<FpsGameWinEvent>.AddListener(OnFpsGameWin);
+            EventBus<FpsGameLoseEvent>.AddListener(OnLose);
         }
 
         protected override void OnExit()
         {
             fpsGame.SetActive(false);
+            EventBus<FpsGameWinEvent>.RemoveListener(OnFpsGameWin);
+            EventBus<FpsGameLoseEvent>.RemoveListener(OnLose);
+        }
+
+        private void OnLose(object sender, FpsGameLoseEvent @event)
+        {
+            StateMachine.TransitionTo(_textAdventureState);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        private void OnFpsGameWin(object sender, FpsGameWinEvent @event)
+        {
+            StateMachine.TransitionTo(_skyrimAdventureState);
+        }
+        
+        IEnumerator WaitAndPrint(float waitTime)
+        {
+            yield return new WaitForSeconds(waitTime);
+            StateMachine.TransitionTo(_skyrimAdventureState);
         }
     }
 }
